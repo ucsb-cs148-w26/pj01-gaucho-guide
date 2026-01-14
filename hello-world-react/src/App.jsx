b53734327e5ba02e1e81dc8295e136a83c9ebc29
Wendy Contreras <wcontrerasmartinez@ucsb.edu>
2026-01-14 12:01:30 -0800

    Add Hello World React app

---
+++ hello-world-react/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World. This is Guacho Guiders.</h1>
+    </div>
+  );
+}
+
+export default App;
