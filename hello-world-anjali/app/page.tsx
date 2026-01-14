568e36168cd29e89b11d42486c3d3e2ec11eb5cd
devanjali97 <devanjali97@gmail.com>
2026-01-14 11:45:10 -0800

    Add Next.js hello world app

---
+++ hello-world-anjali/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black font-sans">
+      <main className="text-center">
+        <h1 className="text-5xl font-bold text-black dark:text-white">
+          Hello World!
+        </h1>
+        
+      </main>
+    </div>
+  );
+}
